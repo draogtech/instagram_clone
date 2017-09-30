80b17f1e009fce635211c77e77be15c1e20ff581
Justin <justinw075@gmail.com>
2017-09-30 23:05:50 +0000

    initial commit

---
+++ post.py
@@ -0,0 +1,18 @@
+import datetime
+
+
+class Post:
+    def __init__(self, title, date_time, content, user_id):
+        pass
+
+    def publish_post(self):
+        pass
+
+    def delete_post(self):
+        pass
+
+    def update_post(self):
+        pass
+
+    def show_all_posts(self):
+        pass
