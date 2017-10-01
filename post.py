54f9cd2020a45d7e7630dbb12b2e9b6c3fdb368b
Justin <justinw075@gmail.com>
2017-10-01 15:19:52 +0000

    added publish_post method

--- post.py
+++ post.py
@@ -1,12 +1,24 @@
 import datetime
+import csv
+import randint
+from random
 
 
 class Post:
-    def __init__(self, title, date_time, content, user_id):
-        pass
+    def __init__(self, title="", date_time="", content="", user_id=""):
+        self.title = title
+        self.date_time = date_time
+        self.content = content
+        self.user_id = user_id
 
     def publish_post(self):
-        pass
+        self.title = input("Please enter the title of your post: ")
+        self.date_time = datetime.datetime.now()
+        self.content = input("Please type your post: ")
+        self.user_id = randint(1000000, 9999999 + 1)
+
+
+        post_file_writer =open('post.csv', 'w', newline='').writerow([self.title, self.date_time, self.content, self.user_id])
 
     def delete_post(self):
         pass
