8bd15147aa8c393f0ee424720a94f46d7204512d
Justin <justinw075@gmail.com>
2017-10-01 16:32:52 +0000

    removed save.py added save-post method in post.py

--- post.py
+++ post.py
@@ -12,10 +12,9 @@ class Post:
 
     def publish_post(self):
 
-
-
         post_file = open('post.csv', 'w', newline='').
         post_reader = csv.writerow([self.title, self.date_time, self.content])
+
 
         post_file.close()
 
