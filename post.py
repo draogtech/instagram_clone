c7e77148dc47c690a65c60dc735a4cec4d6e0236
Justin <justinw075@gmail.com>
2017-10-02 16:15:41 +0000

    save_post method completed, delete post method partially completed

--- post.py
+++ post.py
@@ -1,33 +1,43 @@
 import datetime
 import csv
-
-
+import random
 
 
 class Post:
-    def __init__(self, title="", date_time="", content="":
+    def __init__(self, title, content):
         self.title = title
-        self.date_time = date_time
+
+        self.date_time = datetime.datetime.now()
         self.content = content
+        self.user_id = random.randint(1000000, 9999999)
 
-    def publish_post(self):
+    def save_post(self):
+        post_file = open('post.csv', 'a+')
+        fieldnames = ['title', 'date and time', 'content', 'user']
+        writer = csv.DictWriter(post_file, fieldnames=fieldnames)
 
-        post_file = open('post.csv', 'w', newline='').
-        post_reader = csv.writerow([self.title, self.date_time, self.content])
-
+        writer.writerow(
+            {'title': self.title, 'date and time': self.date_time, 'content': self.content, 'user': self.user_id, })
 
         post_file.close()
 
-    def delete_post(self):
-        post_file = open('post.csv')
-        post_reader = csv.reader(post_file)
-        title_to_delete = input("Enter title of the post you want to delete: ")
 
-        for row in post_reader:
-            if title_to_delete in post == True
+def delete_post(self):
+    post_file = open('post.csv')
+    post_reader = csv.DictReader(post_file)
+    title_to_delete = input("Enter title of the post you want to delete: ")
 
+    for row in post_reader:
+        if title_to_delete in post == row['title']:
+                
+
+'''
     def update_post(self):
         pass
 
     def show_all_posts(self):
-        pass
+        pass'''
+
+g = Post("gghh", "wow!")
+
+g.save_post()
