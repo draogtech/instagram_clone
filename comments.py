d0c89b0410b3a903ae0cd79a34098cc6e9ccea24
pokafor <precious.eb.okafor@gmail.com>
2017-10-02 16:32:54 +0000

    Updated comments class which has also  been added to the oriinal precious file

--- comments.py
+++ comments.py
@@ -1,50 +1,32 @@
+import csv
+import datetime
+
+
 class Comments:
     def __init__(self, comment={}):
-
         self.comment = comment
+        self.time = datetime.datetime.now()
 
+    def add_comment(self, user_comment):
+        self.user_id
+        self.comment[self.user_id] = [user_comment]
+        # for user_id in self.comment.keys():
+        #     print(user_id)
 
-    def add_comment(self):
-        iduser = input("Enter your ID")
-        yourcomment = input("Enter your Comment")
-        youremail = input("Enter your Email")
+    def show_all(self):
+        print(self.comment)
 
-        self.comment.setdefault(iduser,[]).append(yourcomment)
-        self.comment[iduser].append(youremail)
+    def remove(self, user_comment):
 
+        if user_comment in self.comment.keys():
+            del self.comment[user_comment]
+            print("Comment delete ")
 
-        for iduser in self.comment.keys():
-            print(iduser)
-
-    def search(self,find):
-
-        if find in self.comment.keys():
-            look = self.comment[find]
-            print ("yourcomment: " + look[0])
-
-    def remove(self,rmve):
-
-        if rmve in self.comment.keys():
-            del self.comment[rmve]
-            print ("Comment delete ")
-
-new = Comments()
-
-check = input("To add comment press 1, To search comment press 2, To delete comment press 3, To exit press 4:  " )
-
-
-while check != "4":
-
-    if check == "1":
-        new.add_comment()
-
-    elif check == "2":
-        new.search(input("Enter iduser : "))
-
-    elif check == "3":
-        new.remove(input("Enter iduser: "))
-
-    check = input("To add comment press 1, To search comment press 2, To delete comment press 3, To exit press 4:  ")
+    def save(self):
+        save_comment = open('comments.csv', 'a+')
+        field_names = ["user_id", "comment", "time"]
+        comments = csv.DictWriter()
+        comments.writerow({"user_id":self.user_id, "comment":self.comment, "time":self.time})
 
 
 
--- precious.py
+++ precious.py
@@ -1,9 +1,39 @@
+import csv
+import datetime
+
+
 class Like:
     def __init__(self):
         pass
+
+
 class Comment:
-    def __init__(self):
-        pass
+    def __init__(self, comment={}):
+        self.comment = comment
+        self.time = datetime.datetime.now()
+
+    def add_comment(self, user_comment):
+        self.user_id
+        self.comment[self.user_id] = [user_comment]
+        # for user_id in self.comment.keys():
+        #     print(user_id)
+
+    def show_all(self):
+        print(self.comment)
+
+    def remove(self, user_comment):
+
+        if user_comment in self.comment.keys():
+            del self.comment[user_comment]
+            print("Comment delete ")
+
+    def save(self):
+        save_comment = open('comments.csv', 'a+')
+        field_names = ["user_id", "comment", "time"]
+        comments = csv.DictWriter()
+        comments.writerow({"user_id": self.user_id, "comment": self.comment, "time": self.time})
+
+
 class Share:
     def __init__(self):
         pass
