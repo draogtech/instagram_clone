e407831b8503d201bd33b56a6e2fac8d580a6db8
Justin <justinw075@gmail.com>
2017-10-03 10:31:30 +0000

    completed show_all_posts method

--- post.py
+++ post.py
@@ -13,35 +13,40 @@ class Post:
 
     def save_post(self):
         post_file = open('post.csv', 'a+')
-        fieldnames = ['title', 'date and time', 'content', 'user', 'post_id']
+        fieldnames = ['title', 'date and time', 'content', 'post id']
         writer = csv.DictWriter(post_file, fieldnames=fieldnames)
 
         writer.writerow(
-            {'title': self.title, 'date and time': self.date_time, 'content': self.content, 'user': self.user_id, 'post id': self.post_id})
+            {'title': self.title, 'date and time': self.date_time, 'content': self.content, 'post id': self.post_id})
 
         post_file.close()
 
+    '''def delete_post(self):
+        post_file = open('post.csv')
+        post_reader = csv.DictReader(post_file)
+        title_to_delete = input("Enter title of the post you want to delete: ")
 
-def delete_post(self):
-    post_file = open('post.csv')
-    post_reader = csv.DictReader(post_file)
-    title_to_delete = input("Enter title of the post you want to delete: ")
-
-    for row in post_reader:
-        if title_to_delete in post_reader == row['title']:
-
-
-'''
-    def update_post(self):
-        pass
-        '''
+        for row in post_reader:
+            if title_to_delete in post_reader == row['title']:'''
 
     def show_all_posts(self):
         post_file = open('post.csv')
         post_reader = csv.reader(post_file)
-        for row in post_reader:
-        print('Post #' + str(post_reader.line_num) + ' ' + str(row))
+        post_data = list(post_reader)
+        print(post_data)
+
+        for i in post_data:
+            print('******************************************************')
+            for j in range(len(i) - 1):
+                print(i[j])
+
+    def find_post(self):
+        
+
+
 
 g = Post("gghh", "wow!")
 
 g.save_post()
+
+g.show_all_posts()
