f85ef82bc4fbcbd3fc5e1521dc2c70edbc6bef34
pokafor <precious.eb.okafor@gmail.com>
2017-10-03 11:19:08 +0000

    Dexter's like code

--- likeComment.py
+++ likeComment.py
@@ -1,16 +1,42 @@
-class LikeComment():
+import datetime
+
+
+class likeComment():
     def __init__(self):
         self.like = False
+        self.time = ''
 
-    def like_is_ok(self):
-        self.like = True
+    # def like_is_ok(self):
+    #     self.like = True
+    #     return self.like
+    #
+    # def like_is_notok(self):
+    #     self.like = False
+    #     return self.like
+
+    def condition_is_ok_or_nok(self):
+        self.time = datetime.datetime.now()
+
+        if self.like == True:
+            self.like = False
+        else:
+            self.like = True
+            print(self.time)
+
         return self.like
 
-    def like_is_notok(self):
-        self.like = False
-        return self.like
+
+    # def my_time(self):
+    #
+    #     self.time = datetime.datetime.now()
+    #     return self.time
+
+
+
 
 if __name__ == "__main__":
-    like_comment = LikeComment()
+    like_comment = likeComment()
+    print(like_comment.condition_is_ok_or_nok())
+    # print(like_comment.my_time())
 
-    print(like_comment.like_is_notok())
+
